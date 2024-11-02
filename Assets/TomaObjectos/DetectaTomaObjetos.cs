18024c62a8fd159ccdf93ac029694da18dc5c095
AndresG191 <andresrgomez191@gmail.com>
2024-11-01 21:56:11 -0600

    up

--- Assets/TomaObjectos/DetectaTomaObjetos.cs
+++ Assets/TomaObjectos/DetectaTomaObjetos.cs
@@ -9,12 +9,15 @@ public class DetectaTomaObjetos : MonoBehaviour
     public GameObject tomado;
     public bool isTaken;
     public bool estaElObjetoCerca;
+
     public Vector3 original_scale;
 
     Transform padre;
-    private void Awake(){
-        padre = GameObject.Find("Capsule").GetComponent<Transform>();
+
+    private void Awake() {
+        padre = GameObject.Find("Personaje").GetComponent<Transform>();
     }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,68 +28,55 @@ public class DetectaTomaObjetos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-         //tomado = temporal;
-            if(Input.GetKeyDown(KeyCode.P)) {
-                if(!isTaken) {
-                    isTaken = true;
-                } else {
-                    isTaken = false;
+        if(Input.GetKeyDown(KeyCode.P)){
+                if(!isTaken){
+                    isTaken = true;                    
+                }
+                else{
+                    isTaken = false;                    
                 }
             }
     }
 
-    private void OnTriggerEnter(Collider other){
+    private void OnTriggerEnter(Collider other) {
         GameObject temporal = other.gameObject;
-        if(temporal.CompareTag("ObjetoTomable")) {
-        estaElObjetoCerca = true; 
+        if(temporal.CompareTag("ObjetoTomable")){  
+            estaElObjetoCerca = true; //variable que puede ser alterada por algun evento externo
+            //como podria ser una corrutina
         }
-        
     }
 
-
-      private void OnTriggerStay(Collider other) {
+    private void OnTriggerStay(Collider other) {
         GameObject temporal = other.gameObject;
-        if(estaElObjetoCerca) {
-           
-            
-            if(Input.GetKeyDown(KeyCode.P)) {//si el obj tomable cerca del personaje
-                if(isTaken) {//y presoono la tecla de tomar
-                     tomado = temporal; //guardo la referencia
-                    isTaken = true;//indico que se tomo
-                    temporal.transform.SetParent(padre);
-                    temporal.transform.position = transform.position;
-                    temporal.transform.rotation = transform.rotation; //asocio el ojbjeto al personaje
+        if(estaElObjetoCerca){  //si esta el objeto tomable cerca del personaje                                      
+                if(isTaken){ //y presiono la tecla de tomar
+                    tomado = temporal;    //guardo la referencia                    
+                    temporal.transform.SetParent(padre); //asocio el objeto al personaje
+                    temporal.transform.position = transform.position; //el objeto se colocara en las manos del personaje                    
+                    temporal.transform.rotation = transform.rotation; //el obj se coloque en la rotacion de las manos
                     original_scale = temporal.transform.localScale;
-                    temporal.transform.localScale = transform.localScale;
+                    temporal.transform.localScale = transform.localScale; 
                     Rigidbody rb = temporal.GetComponent<Rigidbody>();
-                    rb.isKinematic = true; //objeto se vuelve kinematico
+                    rb.isKinematic = true; //obj se vuelve kinematico 
                     rb.useGravity = false;
-                 
-                } else {
-                    tomado = null;
-                    temporal.transform.SetParent(null);
-
+                }
+                else if(tomado!=null){                                         
+                    tomado = null;                    
+                    temporal.transform.SetParent(null);                    
                     Rigidbody rb = temporal.GetComponent<Rigidbody>();
                     rb.useGravity = true;
-                    rb.isKinematic = false;
-                    temporal.transform.localScale = original_scale;
-                    temporal.transform.localScale = original_scale;
-//porque si guarde el original scale porque no se guardo, pregunta para mañana //se pasa por diferencia <-- respuesta
-//pero como lo solucionas-
-                }
-            }
+                    rb.isKinematic = false;                    
+                    temporal.transform.localScale = original_scale;   
+                    //Debug.Log("AAA");                 
+                }            
         }
-       
         Debug.Log(other.gameObject.name);
     }
 
-    
     private void OnTriggerExit(Collider other) {
-        if(other.gameObject.CompareTag("ObjetoTomable")) {
-            tomado = null;
+        if(other.gameObject.CompareTag("ObjetoTomable")){
+            estaElObjetoCerca = false;
         }
         Debug.Log(other.gameObject.name);
     }
 }
-
